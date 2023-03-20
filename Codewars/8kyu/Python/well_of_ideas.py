a339496c12a33cf59559269c7edfa1289937bf97
Leonard Moses <qalexeon@Polaris.local>
2023-03-20 17:08:01 -0400

    initial commit

---
+++ Codewars/8kyu/Python/well_of_ideas.py
@@ -0,0 +1,17 @@
+# def area_or_perimeter(l , w):
+#     # return your answer
+#     if l==w:
+#         result = l*w
+#     else:
+#         result = (l*2)+(w*2)
+#     print(result)
+
+# area_or_perimeter(5,6)
+
+
+def remove(s):
+    s = "!hi!"
+    s2 = s.find("!")
+    print(s2) 
+
+remove("!hi!")
